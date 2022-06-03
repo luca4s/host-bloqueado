f3f125b56a6e1bf229e6cc94bdb9fd7c11dc7f3d
Lucas <96322105+luca4s@users.noreply.github.com>
2022-06-02 23:31:29 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const hosts = [
+    "gartic",
+    "poki",
+    "friv",
+    "clickjogos",
+    "stopots",
+    "roblox",
+    "minijogos"
+];
+hosts.forEach(function (v, i) {
+    if (window.location.host.match(v)) {
+        document.head.innerHTML = '<title>Host bloqueado.</title> <style>body { text-align: center; font-family: Arial, Helvetica, sans-serif; }</style>'
+        document.body.innerHTML = '<b class="title">Host bloqueado.</b><br><a>Este host foi bloqueado pela biblioteca por motivos de uso impróprio.</a><br><a>Por favor, apenas use os computadores para motivos educacionais.</a>'
+    }
+});
